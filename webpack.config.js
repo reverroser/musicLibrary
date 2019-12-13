1bae9f4a1d15c5b7fdf5f9506cb4fe6e65d944d3
Roser Reverte <reverroser@gmail.com>
2019-12-13 15:31:18 +0100

    sass and jquery setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssPackPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    }
+                ]
+            },
             {
                 test: /\.html$/,
                 use: [
@@ -14,12 +24,34 @@ module.exports = {
                     }
                 ]
             },
+            {
+                test: /\.s(a|c)ss$/,
+                use: [
+                    'style-loader', // Creates `style` nodes from JS strings
+                    'css-loader', // Translates CSS into CommonJS
+                    'sass-loader', // Compiles Sass to CSS
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: './src/index.html',
             filename: './index.html'
+        }),
+        new MiniCssPackPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
         })
     ]
 }
