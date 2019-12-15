1c1732bd20f451eb24500827d77465ded964f0fc
Roser Reverte <reverroser@gmail.com>
2019-12-15 14:48:43 +0100

    get input value on type

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -4,11 +4,19 @@ class SearchInput extends HTMLElement {
     constructor() {
         super();
         this.render();
+
+        // Everytime the user types, .onInput is called
+        $('#searchInput')[0].addEventListener('input', this.onInput);
+    }
+
+    onInput(event) {
+        // This will print the value of the input
+        console.log(event.target.value);
     }
 
     render() {
         this.innerHTML = `
-            <input class="search-input" placeholder="Search" />
+            <input type="text" id="searchInput" class="search-input" placeholder="Search" />
         `;
     }
 }
